56efd2561e231cc7e95804b59632e42955fdcfce
Sharmelyn <extsbautista@utp.edu.pe>
2024-07-03 00:44:08 -0500

    feat(boton): creacion de btoon de inicio

--- src/components/Level1/TopBar/index.jsx
+++ src/components/Level1/TopBar/index.jsx
@@ -46,6 +46,7 @@ const TopBar = () => {
               zIndex: "100",
               top: "0",
               paddingLeft: "5px",
+              textAlign: "center",
             }}
           >
             {" "}
